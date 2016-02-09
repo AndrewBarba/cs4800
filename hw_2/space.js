0606177501688efe67d01b84338b0942cbeae4d1
Andrew Barba <abarba.77@gmail.com>
2016-02-08 23:31:15 -0500

    cleanup

--- hw_2/space.js
+++ hw_2/space.js
@@ -9,6 +9,8 @@
  *----------------------------------------------------------------------------*/
 
 function processData(input) {
+  let start = Date.now();
+
   let parts = input.split('\n').map(line => line.split(' ').map(num => parseInt(num)));
 
   let species = [];
@@ -20,27 +22,73 @@ function processData(input) {
     cost: parts[2][i]
   }));
 
-  species.forEach((s, j) => {
-    for (let i = 1; i <= s.count; i++) {
-      let carry = [];
-      for (let k = 0; k < species.length; k++) {
-        carry.push(k === j ? i : 0);
-      }
-      let d = {
-        cost: i * s.cost,
-        carry: carry
-      };
-      discounts.push(d);
-    }
-  });
-
   // Parse discounts
   parts.slice(3).forEach(flight => discounts.push({
     cost: flight[0],
     carry: flight.slice(1)
   }));
 
-  console.log(discounts);
+  // Calculate price without a discount
+  let answer = species.reduce((prev, cur) => prev + (cur.cost * cur.count), 0);
+
+  // Duplicate discounts
+  let len = discounts.length;
+  for (let i = 0; i < len; i++) {
+    try {
+      priceWithDiscount(combinedDiscount([discounts[i], discounts[i]]));
+    } catch(err) { continue; }
+    discounts.push(discounts[i]);
+  }
+
+  // Sort discounts
+  discounts = discounts.sort((a, b) => a.cost > b.cost);
+
+  // Calculate all prices with a discount
+  var ps = [[]];
+  for (let i=0; i < discounts.length; i++) {
+    for (let j = 0, len = ps.length; j < len; j++) {
+      if (Date.now() - start > 8 * 1000) {
+        console.log(answer);
+        return;
+      }
+      let ds = ps[j].concat(discounts[i]);
+      let d = combinedDiscount(ds);
+      if (answer <= d.cost) continue;
+      if (d.carry === parts[1]) {
+        console.log(d.cost);
+        return;
+      }
+      let p;
+      try {
+        p = priceWithDiscount(d);
+      } catch(err) {
+        continue;
+      }
+      answer = p < answer ? p : answer;
+      ps.push(ds);
+    }
+  }
+
+  console.log(answer);
+
+  function priceWithDiscount(discount) {
+    return species.reduce((prev, cur, i) => {
+      if (cur.count < discount.carry[i]) throw new Error('Invalid discount');
+      return prev + (cur.cost * (cur.count - discount.carry[i]));
+    }, discount.cost);
+  }
+
+  function combinedDiscount(discounts) {
+    return discounts.reduce((prev, cur) => {
+      return {
+        cost: prev.cost + cur.cost,
+        carry: prev.carry.map((num, i) => num + cur.carry[i])
+      };
+    }, {
+      cost: 0,
+      carry: species.map(() => 0)
+    });
+  }
 }
 
 /*----------------------------------------------------------------------------*
