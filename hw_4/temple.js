d3b5319226daaa04bb73821eff8a0c4d0c47367c
Andrew Barba <abarba.77@gmail.com>
2016-02-28 23:45:01 -0500

    start hw4

---
+++ hw_4/temple.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/*----------------------------------------------------------------------------*
+ * Parse Data
+ *----------------------------------------------------------------------------*/
+
+function processData(input) {
+  const parts = input.split('\n').map(line => line.split(' ').map(n => parseInt(n)));
+  const n = parts[0][0];
+  const m = parts[1][0];
+  const k = parts[0].slice(1).reverse();
+  const l = parts[1].slice(1).reverse();
+  const d = n + m - 1;
+  const c = [];
+
+  for (let t = 0; t < d; t++) {
+    let key = 0;
+    let start = Math.max(0, t - m + 1);
+    let end = Math.min(n - 1, t);
+    for (let i = start; i <= end; i++) {
+      key += k[i] * l[t - i];
+    }
+    c.push(key);
+  }
+
+  console.log(c.reverse().join(' '));
+}
+
+/*----------------------------------------------------------------------------*
+ * Read Data
+ *----------------------------------------------------------------------------*/
+
+let input = "";
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+process.stdin.on('data', data => input += data);
+process.stdin.on('end', () => processData(input.trim()));
